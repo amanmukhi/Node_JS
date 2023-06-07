1300b2916836394833d5c7ecea609e09dff17df1
Aman Kumar Mukhi <65829037+amanmukhi@users.noreply.github.com>
2023-06-07 18:55:23 +0530

    update node js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-   
+module.exports={
+	x:10,
+	y:20,
+	z:function(){
+		return 10;
+	}
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+// let a = 10;
+const arr=[2,4,6,2,4,6,3,0];
+
+arr.filter((item)=>{
+	console.log(item)
+})
+// console.log(app.z());
